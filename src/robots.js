9822c09c8a07f2db4d179b6fde1054f6d3bc540c
Kaio Rosa <kaiosrosa@gmail.com>
2019-12-24 11:42:31 -0300

    add progress robotfriends

---
+++ src/robots.js
@@ -0,0 +1,17 @@
+export const robots =[
+    {
+        id: 1,
+        name: "Kaio Rosa",
+        email:"kaiosrosa@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Kamila Rosa",
+        email:"htonaquedetona@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Karen Rosa",
+        email:"karen.penguin@gmail.com"
+    }
+]
