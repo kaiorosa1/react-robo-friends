98662aed3b4783737ce659d642c618cd5f2bd2ff
Kaio Rosa <kaiosrosa@gmail.com>
2019-12-25 19:00:04 -0300

    add loop to display cardcards

--- src/robots.js
+++ src/robots.js
@@ -13,5 +13,33 @@ export const robots =[
         id: 3,
         name: "Karen Rosa",
         email:"karen.penguin@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Luana Rosa",
+        email:"lu.penguin@gmail.com"
+    },
+    {
+        id: 5,
+        name: "Karina Rosa",
+        email:"karina@gmail.com"
+    },
+    {
+        id: 6,
+        name: "Peter Rosa",
+        email:"peter@gmail.com"
+    }
+    , {
+        id: 7,
+        name: "Jocilia Rosa",
+        email:"jojo@gmail.com"
+    }, {
+        id: 8,
+        name: "Claudio Rosa",
+        email:"ca.orosa@gmail.com"
+    },  {
+        id: 9,
+        name: "Julia Rosa",
+        email:"juh.rosa@gmail.com"
     }
 ]
