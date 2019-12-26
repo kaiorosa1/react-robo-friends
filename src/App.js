f4351afa176c91a5a3686c6c51259a35901e9aea
Kaio Rosa <kaiosrosa@gmail.com>
2019-12-26 10:30:04 -0300

    add robofriends complete

--- src/App.js
+++ src/App.js
@@ -2,17 +2,37 @@ import React from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import { robots } from './robots'
+import './App.css'
 
-const App = () => {
-    return(
-        <div className="tc">
-            <div>
-                <h1>Robot Friends</h1>
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+
+    }
+    render() {
+
+        const filterRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+
+        return (
+            <div className="tc">
+                <div>
+                    <h1 className="f1">Robot Friends</h1>
+                </div>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filterRobots} />
             </div>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    ) 
+        )
+    }
 }
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const SearchBox = () =>{
+const SearchBox = (props) => {
     return (
         <div className="pa2">
-            <input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="search robos"/>
+            <input
+                className="pa3 ba b--green bg-lightest-blue"
+                type="search"
+                placeholder="search robos"
+                onChange={props.searchChange}
+            />
         </div>
     );
 }
