3a3b7a7fcdf3c111ff425614ad7688a5b4fe73b2
Kaio Rosa <kaiosrosa@gmail.com>
2019-12-27 20:15:51 -0300

    refactoring code

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Scroll = (props) =>{
     return (
-        <div>
+        <div style={{overflowY: 'scroll', border: '1px solid black', height: '500px'}}>
             {props.children}
         </div>
        
