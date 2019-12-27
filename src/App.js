7c7b5c1bb36fc1f3c374ddad86fb6bd4282944d8
Kaio Rosa <kaiosrosa@gmail.com>
2019-12-27 20:03:37 -0300

    add lifecycle componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
-import { robots } from './robots'
+import Scroll from './Scroll'
 import './App.css'
+
 
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+    }
+
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value });
 
@@ -28,8 +35,11 @@ class App extends React.Component {
                 <div>
                     <h1 className="f1">Robot Friends</h1>
                 </div>
+                
                 <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
                 <CardList robots={filterRobots} />
+                </Scroll>
             </div>
         )
     }
--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Card = (props) =>{
     return (
     <div className="tc bg-light-green dib pa3 ma2 grow bw2 shadow-5">
         <div>
-            <img src={`https://robohash.org/${props.id}?200x200`} alt="robots"/>
+            <img src={`https://robohash.org/${props.id}?size=300x300`} alt="robots"/>
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
