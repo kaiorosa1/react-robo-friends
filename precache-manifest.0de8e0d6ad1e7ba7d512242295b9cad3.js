754b47f950039fec3a802a6a8dcac1f68534e4de
Kaio Rosa <kaiosrosa@gmail.com>
2019-12-28 11:29:56 -0300

    Updates

---
+++ precache-manifest.0de8e0d6ad1e7ba7d512242295b9cad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca071ea6e19fe1c1a7029ccc8bfa7a5",
+    "url": "/react-robo-friends/index.html"
+  },
+  {
+    "revision": "449890cabe2ea1118d2d",
+    "url": "/react-robo-friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "796389c6150e5be7c9c7",
+    "url": "/react-robo-friends/static/css/main.1f23aa73.chunk.css"
+  },
+  {
+    "revision": "449890cabe2ea1118d2d",
+    "url": "/react-robo-friends/static/js/2.f0d44eb8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-robo-friends/static/js/2.f0d44eb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "796389c6150e5be7c9c7",
+    "url": "/react-robo-friends/static/js/main.2fc25a52.chunk.js"
+  },
+  {
+    "revision": "46fb15067a6374cbbda2",
+    "url": "/react-robo-friends/static/js/runtime-main.206db1cb.js"
+  }
+]);
